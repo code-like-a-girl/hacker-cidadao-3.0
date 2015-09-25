4ad9f17b862a45dfb7bb3111a3f64f548f50f6e1
ThaisaMirely <mirelythaisa@gmail.com>
2015-09-25 14:58:45 -0300

    Wireframe - Version 1.0
    Wireframe in html format

---
+++ Design/Version 1.0/Wireframe/Storyboard_files/resources/story.js
@@ -0,0 +1,193 @@
+var story = {
+ "pages": [
+  {
+   "image": "initial_empty.png",
+   "width": 406,
+   "links": [
+    {
+     "rect": [
+      349,
+      107,
+      373,
+      131
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      205,
+      146,
+      385,
+      194
+     ],
+     "page": 3
+    }
+   ],
+   "title": "initial_empty",
+   "height": 794
+  },
+  {
+   "image": "initial_loading.png",
+   "width": 406,
+   "links": [
+    {
+     "rect": [
+      203,
+      144,
+      383,
+      192
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      68,
+      351,
+      348,
+      443
+     ],
+     "page": 2
+    }
+   ],
+   "title": "initial_loading",
+   "height": 794
+  },
+  {
+   "image": "initial_populated.png",
+   "width": 406,
+   "links": [
+    {
+     "rect": [
+      347,
+      107,
+      371,
+      131
+     ],
+     "page": 1
+    },
+    {
+     "rect": [
+      203,
+      146,
+      383,
+      194
+     ],
+     "page": 3
+    }
+   ],
+   "title": "initial_populated",
+   "height": 794
+  },
+  {
+   "image": "category_list.png",
+   "width": 406,
+   "links": [
+    {
+     "rect": [
+      344,
+      235,
+      377,
+      275
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      348,
+      319,
+      381,
+      359
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      307,
+      576,
+      365,
+      634
+     ],
+     "page": 4
+    },
+    {
+     "rect": [
+      23,
+      142,
+      203,
+      190
+     ],
+     "page": 2
+    }
+   ],
+   "title": "category_list",
+   "height": 794
+  },
+  {
+   "image": "edit_add_category.png",
+   "width": 406,
+   "links": [
+    {
+     "rect": [
+      312,
+      568,
+      366,
+      622
+     ],
+     "page": 3
+    },
+    {
+     "rect": [
+      23,
+      146,
+      203,
+      194
+     ],
+     "page": 2
+    },
+    {
+     "rect": [
+      203,
+      146,
+      383,
+      194
+     ],
+     "page": 3
+    }
+   ],
+   "title": "edit_add_category",
+   "height": 794
+  },
+  {
+   "image": "notification.png",
+   "width": 456,
+   "links": [{
+    "rect": [
+     21,
+     101,
+     431,
+     175
+    ],
+    "page": 6
+   }],
+   "title": "notification",
+   "height": 872
+  },
+  {
+   "image": "initial_populated_by_notification.png",
+   "width": 406,
+   "links": [{
+    "rect": [
+     211,
+     146,
+     391,
+     194
+    ],
+    "page": 3
+   }],
+   "title": "initial_populated_by_notification",
+   "height": 794
+  }
+ ],
+ "title": "Storyboard",
+ "highlightLinks": false
+}
